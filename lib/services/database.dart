79688755dbf6f7c5ca209ab568eb34d4fbaa1a3a
nghiapd17012003 <45303883+nghiapd17012003@users.noreply.github.com>
2024-08-05 11:19:28 +1200

    feat: add attendee firestore stream

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -7,10 +7,10 @@ class DataBaseService{
   DataBaseService({required this.uid});
 
   //collection reference
-  final CollectionReference attendee = FirebaseFirestore.instance.collection('attendee');
+  final CollectionReference attendeeCollection = FirebaseFirestore.instance.collection('attendee');
 
   Future updateUserData(String fname, String lname, int tokenAmount) async{
-    return await attendee.doc(uid).set({
+    return await attendeeCollection.doc(uid).set({
       'fname' : fname,
       'lname' : lname,
       'tokenAmount' : tokenAmount,
@@ -19,6 +19,6 @@ class DataBaseService{
 
   //get attendee stream
   Stream<QuerySnapshot> get attendees{
-    return attendee.snapshots();
+    return attendeeCollection.snapshots();
   }
 }
