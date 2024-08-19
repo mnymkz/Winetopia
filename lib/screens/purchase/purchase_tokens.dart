325601bff9b26a427c952543ea5b5a0a9c14d1a3
mnymkz <michael.sabalburo5@gmail.com>
2024-08-19 16:53:18 +1200

    refactor: file setup for new purhcase_tokens screen. This commit adds the files for the purchase_tokens screen.

---
+++ lib/screens/purchase/purchase_tokens.dart
@@ -0,0 +1,16 @@
+//purchase_tokens screen contains the screen with the purchase token button
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
