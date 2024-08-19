2abeb0cd697e6df170a8f9d90671d25bcff80d0d
mnymkz <michael.sabalburo5@gmail.com>
2024-08-19 17:09:31 +1200

    refactor: stripe project setup. This commit makes changes to the android files based on the instruction from the flutter app sdk website to add stripe payments to the project.

--- lib/screens/purchase/purchase_tokens.dart
+++ lib/screens/purchase/purchase_tokens.dart
@@ -1,16 +1,22 @@
 //purchase_tokens screen contains the screen with the purchase token button
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
-
-  @override
-  State<MyWidget> createState() => _MyWidgetState();
-}
-
-class _MyWidgetState extends State<MyWidget> {
+class PurchaseTokensScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Purchase Tokens'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Placeholder action
+            print('Purchase Tokens button pressed');
+          },
+          child: Text('Purchase Tokens'),
+        ),
+      ),
+    );
   }
 }
