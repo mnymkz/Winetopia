55280448e597b042a66dde0cf9f473153f3f5173
Teresa <teresamatalavea@yahoo.com>
2024-09-13 10:42:36 +1200

    fix: fixed incorrect widget name
    NewScreen() changed to ProfileScreen() in navigation screen

--- lib/screens/navigation.dart
+++ lib/screens/navigation.dart
@@ -29,75 +29,73 @@ class _NavigationScreenState extends State<NavigationScreen> {
     HomeScreen(), // navigates to home screen (nfc scanning)
     HomeScreen(), // change route to history
     HomeScreen(), // change route to event info
-    NewScreen(), // navigates to profile screen
+    ProfileScreen(), // navigates to profile screen
   ];
 
   @override
   Widget build(BuildContext context) {
-    return _auth.checkVerifyEmail() ? Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Winetopia',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.deepPurple.shade400,
-        elevation: 0.0,
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(
-              Icons.person,
-              color: Colors.white,
-            ),
-            onPressed: () {
-              // Navigate to the profile screen
-              Navigator.push(
-                context,
-                PageRouteBuilder(
-                  pageBuilder: (context, animation, secondaryAnimation) => NewScreen(),
-                  transitionsBuilder: (context, animation, secondaryAnimation, child) {
-                    const begin = Offset(1.0, 0.0); // Start from the right (1.0 means off-screen right)
-                    const end = Offset.zero; // End at the center (normal position)
-                    const curve = Curves.easeInOut;
+    return _auth.checkVerifyEmail()
+        ? Scaffold(
+            appBar: AppBar(
+              title: const Text(
+                'Winetopia',
+                style: TextStyle(color: Colors.white),
+              ),
+              backgroundColor: Colors.deepPurple.shade400,
+              elevation: 0.0,
+              actions: <Widget>[
+                IconButton(
+                  icon: const Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    // Navigate to the profile screen
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        pageBuilder: (context, animation, secondaryAnimation) =>
+                            ProfileScreen(),
+                        transitionsBuilder:
+                            (context, animation, secondaryAnimation, child) {
+                          const begin = Offset(1.0,
+                              0.0); // Start from the right (1.0 means off-screen right)
+                          const end = Offset
+                              .zero; // End at the center (normal position)
+                          const curve = Curves.easeInOut;
 
-                    var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-                    var offsetAnimation = animation.drive(tween);
+                          var tween = Tween(begin: begin, end: end)
+                              .chain(CurveTween(curve: curve));
+                          var offsetAnimation = animation.drive(tween);
 
-                    return SlideTransition(
-                      position: offsetAnimation,
-                      child: child,
+                          return SlideTransition(
+                            position: offsetAnimation,
+                            child: child,
+                          );
+                        },
+                        transitionDuration: const Duration(
+                            milliseconds: 300), // Customize duration if needed
+                      ),
                     );
                   },
-                  transitionDuration: const Duration(milliseconds: 300), // Customize duration if needed
                 ),
-              );
-            },
-          ),
-        ],
-      ),
+              ],
+            ),
 
-      body: _screens[_selectedIndex],
+            body: _screens[_selectedIndex],
 
-      //backgroundColor: Colors.white,
-      bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: Colors.purple.shade50,
-        color: Colors.deepPurple.shade400,
-        animationDuration: Duration(milliseconds: 200),
-        onTap: _navigateCurvedBar,
-        items: [
-          Icon(
-            Icons.home,
-            color: Colors.white
-          ),
-          Icon(
-            Icons.history,
-            color: Colors.white
-          ),
-          Icon(
-            Icons.calendar_month,
-            color: Colors.white
-          ),
-        ]
-      ),
-    ) : VerifyEmail();
+            //backgroundColor: Colors.white,
+            bottomNavigationBar: CurvedNavigationBar(
+                backgroundColor: Colors.purple.shade50,
+                color: Colors.deepPurple.shade400,
+                animationDuration: Duration(milliseconds: 200),
+                onTap: _navigateCurvedBar,
+                items: [
+                  Icon(Icons.home, color: Colors.white),
+                  Icon(Icons.history, color: Colors.white),
+                  Icon(Icons.calendar_month, color: Colors.white),
+                ]),
+          )
+        : VerifyEmail();
   }
 }
