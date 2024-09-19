09fecdabfe20c1b6132548cacd69196f75d1c07e
nghiapd17012003 <45303883+nghiapd17012003@users.noreply.github.com>
2024-09-20 10:37:29 +1200

    feat: update the rest of Winetopia schedule

--- lib/screens/schedule.dart
+++ lib/screens/schedule.dart
@@ -137,12 +137,20 @@ class _ScheduleState extends State<Schedule> {
                 //subtitle: Text('EXPERT-LED MASTERCLASSES RUN FOR 30 - 40 MINUTES TASTING 4 PREMIUM WINES. LOCATED ON LEVEL 3.'),
                 children: [
                   ListTile(
-                    title: Text('6.30 - 7.10PM'),
-                    subtitle: Text('Wine & Food Pairing with Duo Eatery'),
+                    title: Text('11.45 - 12.25PM'),
+                    subtitle: Text('Wine & Food Pairing with Culprit'),
                   ),
                   ListTile(
-                    title: Text('7.30 - 8.00PM'),
-                    subtitle: Text('Great Whites, Decadent Reds'),
+                    title: Text('12.45 - 1.15PM'),
+                    subtitle: Text('Dream Room Masterclass with Mike Bennie'),
+                  ),
+                  ListTile(
+                    title: Text('1.35 - 2.05PM'),
+                    subtitle: Text('Let’s get Fizzed - Top NZ Methode with Angela Allan'),
+                  ),
+                  ListTile(
+                    title: Text('2:25 - 3.05PM'),
+                    subtitle: Text('Wine & Food Pairing with Manzo'),
                   ),
                 ],
               )
@@ -153,7 +161,75 @@ class _ScheduleState extends State<Schedule> {
             title: Text('Session 3: SATURDAY 5PM - 8.30PM'),
             //subtitle: Text('Trailing expansion arrow icon'),
             children: <Widget>[
-              ListTile(title: Text('This is tile number 1')),
+              //ListTile(title: Text('This is tile number 1')),
+              ExpansionTile(
+                title: Text('MAIN STAGE'),
+                subtitle: Text('HOSTED BY KEELAN MCCAFFERTY'),
+                children: [
+                  ListTile(
+                    title: Text('5.15PM'),
+                    subtitle: Text('Tickle Me Pink with Mike Bennie'),
+                  ),
+                  ListTile(
+                    title: Text('5.50PM'),
+                    subtitle: Text('M Share A Glass with Matt Heath, Broadcaster & Author of “A Life Less Punishing”'),
+                  ),
+                  ListTile(
+                    title: Text('6.25PM'),
+                    subtitle: Text('Pinot vs Pinot Wine Battle: Marlborough vs Central Otago'),
+                  ),
+                  ListTile(
+                    title: Text('7.00PM'),
+                    subtitle: Text('M Winetopia Dream Room with Mike Bennie & Jordyn Harper, Winemaker of Craggy Range'),
+                  ),
+                  ListTile(
+                    title: Text('7.35PM'),
+                    subtitle: Text('Live Music by The Tiny Tickets'),
+                  ),
+                ],
+              ),
+      
+              ExpansionTile(
+                title: Text('AMERICAN EXPRESS PREMIUM LOUNGE'),
+                subtitle: Text('HOSTED BY MERMAID MARY'),
+                children: [
+                  ListTile(
+                    title: Text('6.00PM'),
+                    subtitle: Text('Discover Cloudy Bay'),
+                  ),
+                  ListTile(
+                    title: Text('6.45PM'),
+                    subtitle: Text('Discover Esses'),
+                  ),
+                  ListTile(
+                    title: Text('7.30PM'),
+                    subtitle: Text('Discover Moy Hall'),
+                  ),
+                ],
+              ),
+      
+              ExpansionTile(
+                title: Text('MASTERCLASSES'),
+                //subtitle: Text('EXPERT-LED MASTERCLASSES RUN FOR 30 - 40 MINUTES TASTING 4 PREMIUM WINES. LOCATED ON LEVEL 3.'),
+                children: [
+                  ListTile(
+                    title: Text('4.45 - 5.25PM'),
+                    subtitle: Text('Wine & Food Pairing with Te Kaahu'),
+                  ),
+                  ListTile(
+                    title: Text('5.45 - 6.15PM'),
+                    subtitle: Text('Round the World in Rouge - Top Reds with Mike Bennie'),
+                  ),
+                  ListTile(
+                    title: Text('6.35 - 7.05PM'),
+                    subtitle: Text('The Great Kiwi (Wine) Roadtrip with Cameron Douglas MS'),
+                  ),
+                  ListTile(
+                    title: Text('7.25 - 8.05PM'),
+                    subtitle: Text('Wine & Food Pairing with Bossi'),
+                  ),
+                ],
+              )
             ],
           ),
         ],
