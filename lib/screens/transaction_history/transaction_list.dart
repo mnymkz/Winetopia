1adbc6b4b0cc8557e9308eff37c8ae3caf3d9275
Teresa <teresamatalavea@yahoo.com>
2024-09-26 19:32:01 +1200

    feat: update transaction history ui (scrollbar)

--- lib/screens/transaction_history/transaction_list.dart
+++ lib/screens/transaction_history/transaction_list.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:winetopia/models/wine_transaction.dart';
@@ -7,13 +8,26 @@ import 'package:winetopia/services/database_service.dart';
 import 'package:winetopia/shared/loading.dart';
 
 /// Widget showing list of wine transactions
-class TransactionList extends StatelessWidget {
-  final int? limit; // Limit to show only a certain number of transactions
-
+class TransactionList extends StatefulWidget {
+  final int? limit;
   const TransactionList({
     super.key,
     this.limit,
   });
+
+  @override
+  State<TransactionList> createState() => _TransactionListState();
+}
+
+class _TransactionListState extends State<TransactionList> {
+  // Limit to show only a certain number of transactions
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,10 +49,11 @@ class TransactionList extends StatelessWidget {
           return const Center(child: Text('No transactions yet'));
         }
 
-        final displayedTransactions =
-            limit != null ? transactions.take(limit!).toList() : transactions;
+        final displayedTransactions = widget.limit != null
+            ? transactions.take(widget.limit!).toList()
+            : transactions;
 
-        return limit != null
+        return widget.limit != null
             ? ListView.builder(
                 physics: const NeverScrollableScrollPhysics(),
                 shrinkWrap: true,
@@ -59,21 +74,25 @@ class TransactionList extends StatelessWidget {
                     color: Colors.transparent,
                   ),
                   Expanded(
-                    child: ListView.builder(
-                      physics: limit != null
-                          ? const NeverScrollableScrollPhysics()
-                          : null,
-                      shrinkWrap: true,
-                      itemCount: displayedTransactions.length,
-                      itemBuilder: (context, index) {
-                        final transaction = displayedTransactions[index];
-                        return Container(
-                          padding: const EdgeInsets.only(bottom: 8.0),
-                          margin: const EdgeInsets.symmetric(horizontal: 15.0),
-                          child: WineTransactionInfoWidget(
-                              transaction: transaction),
-                        );
-                      },
+                    child: CupertinoScrollbar(
+                      thumbVisibility: true,
+                      controller: _scrollController,
+                      child: ListView.builder(
+                        controller: _scrollController,
+                        physics: const AlwaysScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: displayedTransactions.length,
+                        itemBuilder: (context, index) {
+                          final transaction = displayedTransactions[index];
+                          return Container(
+                            padding: const EdgeInsets.only(bottom: 8.0),
+                            margin:
+                                const EdgeInsets.symmetric(horizontal: 15.0),
+                            child: WineTransactionInfoWidget(
+                                transaction: transaction),
+                          );
+                        },
+                      ),
                     ),
                   ),
                 ],
